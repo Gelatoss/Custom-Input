5565fd6d4394af8edc7db6c20b469f6c06d81ce3
Gelatoss <79358673+Gelatoss@users.noreply.github.com>
2025-05-09 16:15:29 +0200

    Updated app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CustomInputComponent } from './component/custom-input/custom-input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CustomInputComponent,
+    CustomInputComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  private fb = inject(NonNullableFormBuilder);
+
   title = 'customInputv2';
+
+  option: string[] = ['option1', 'option2', 'option3'];
+
+  form = this.fb.group({
+    helmet: ['', Validators.required]
+  });  
+
+  onSubmit() {
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      console.log('submitted', this.form.value);
+    }
+  }
 }
